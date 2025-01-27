64648091d9b22937f59504097fb3e22509935501
Hosung <nsa19020@naver.com>
2025-01-28 02:43:18 +0900

    왼쪽 섹션 작업

--- src/app/(beforeLogin)/layout.tsx
+++ src/app/(beforeLogin)/layout.tsx
@@ -10,7 +10,6 @@ type Props = {
 export default function Layout({ children, modal }: Readonly<Props>) {
     return (
         <div className={styles.container}>
-            비포로그인 레이아웃
             {children}
             {modal}
         </div>
