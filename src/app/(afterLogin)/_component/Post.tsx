b40f6e88f4c9457f794906a867564a29ce91a148
Hosung <nsa19020@naver.com>
2025-01-29 16:31:33 +0900

    게시글 섹션 작업

---
+++ src/app/(afterLogin)/_component/Post.tsx
@@ -0,0 +1,62 @@
+import dayjs from 'dayjs';
+import 'dayjs/locale/ko';
+import relativeTime from 'dayjs/plugin/relativeTime';
+import Link from 'next/link';
+
+import style from './post.module.css';
+
+import ActionButtons from '@/app/(afterLogin)/_component/ActionButtons';
+
+dayjs.locale('ko');
+dayjs.extend(relativeTime);
+
+export default function Post() {
+    const target = {
+        User: {
+            id: 'elonmusk',
+            nickname: 'Elon Musk',
+            image: '/profile.jpeg',
+        },
+        content: '클론코딩 라이브로 하니 너무 힘들어요 ㅠㅠ',
+        createdAt: new Date(),
+        Images: [],
+    };
+    return (
+        <article className={style.post}>
+            <div className={style.postWrapper}>
+                <div className={style.postUserSection}>
+                    <Link
+                        href={`/${target.User.id}`}
+                        className={style.postUserImage}
+                    >
+                        <img
+                            src={target.User.image}
+                            alt={target.User.nickname}
+                        />
+                        <div className={style.postShade} />
+                    </Link>
+                </div>
+                <div className={style.postBody}>
+                    <div className={style.postMeta}>
+                        <Link href={`/${target.User.id}`}>
+                            <span className={style.postUserName}>
+                                {target.User.nickname}
+                            </span>
+                            &nbsp;
+                            <span className={style.postUserNick}>
+                                @{target.User.id}
+                            </span>
+                            &nbsp; · &nbsp;
+                        </Link>
+                        <span className={style.postDate}>
+                            {dayjs(target.createdAt).fromNow(true)}
+                        </span>
+                    </div>
+                    <div className={style.postContent}>{target.content}</div>
+                    <div className={style.postImageSection}></div>
+                    <ActionButtons />
+                </div>
+            </div>
+        </article>
+    );
+}
--- src/app/(afterLogin)/home/page.tsx
+++ src/app/(afterLogin)/home/page.tsx
@@ -1,3 +1,29 @@
-export default function Page() {
-    return <div>홈페이지</div>;
+import style from './home.module.css';
+
+import Post from '@/app/(afterLogin)/_component/Post';
+import PostForm from '@/app/(afterLogin)/home/_component/PostForm';
+import Tab from '@/app/(afterLogin)/home/_component/Tab';
+import TabProvider from '@/app/(afterLogin)/home/_component/TabProvider';
+
+export default function Home() {
+    return (
+        <main className={style.main}>
+            <TabProvider>
+                <Tab />
+                <PostForm />
+                <Post />
+                <Post />
+                <Post />
+                <Post />
+                <Post />
+                <Post />
+                <Post />
+                <Post />
+                <Post />
+                <Post />
+                <Post />
+                <Post />
+            </TabProvider>
+        </main>
+    );
 }
