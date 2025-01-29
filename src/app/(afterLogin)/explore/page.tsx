8c3b79ad8b2d54929b5d80db13a51788739077b3
Hosung <nsa19020@naver.com>
2025-01-29 20:39:14 +0900

    search 페이지 작업

--- src/app/(afterLogin)/explore/page.tsx
+++ src/app/(afterLogin)/explore/page.tsx
@@ -1,14 +1,23 @@
 import style from './explore.module.css';
 
+import BackButton from '@/app/(afterLogin)/_component/BackButton';
 import SearchForm from '@/app/(afterLogin)/_component/SearchForm';
 import Trend from '@/app/(afterLogin)/_component/Trend';
 
 export default function Home() {
     return (
         <main className={style.main}>
-            <div className={style.formZone}>
-                <SearchForm />
+            <div className={style.searchTop}>
+                <div className={style.searchZone}>
+                    <div className={style.buttonZone}>{/*<BackButton />*/}</div>
+                    <div className={style.formZone}>
+                        <SearchForm />
+                    </div>
+                </div>
             </div>
+            {/*<div className={style.formZone}>*/}
+            {/*    <SearchForm />*/}
+            {/*</div>*/}
             <div className={style.trend}>
                 <h3>나를 위한 트렌드</h3>
                 <Trend />
