49fdbb232255406a5b97f072a966bc5628cd6a66
Hosung <nsa19020@naver.com>
2025-01-30 20:59:16 +0900

    MSW세팅 전

--- src/app/(beforeLogin)/login/page.tsx
+++ src/app/(beforeLogin)/login/page.tsx
@@ -1,11 +1,16 @@
 'use client';
 
 import { useRouter } from 'next/navigation';
+import { useEffect } from 'react';
 
 import Main from '@/app/(beforeLogin)/_component/Main';
 
 export default function Login() {
     const router = useRouter();
-    router.replace('/i/flow/login');
+
+    useEffect(() => {
+        router.replace('/i/flow/login');
+    }, [router]);
+
     return <Main />;
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,16 +2,6 @@ import React, { ReactNode } from 'react';
 
 import './globals.css';
 
-import { MSWProvider } from '@/app/_component/MSWComponent';
-
-if (
-    process.env.NEXT_RUNTIME === 'nodejs' &&
-    process.env.NODE_ENV !== 'production'
-) {
-    const { server } = require('@/mocks/http');
-    server.listen();
-}
-
 type Props = {
     children: ReactNode;
 };
@@ -19,9 +9,7 @@ type Props = {
 export default function RootLayout({ children }: Readonly<Props>) {
     return (
         <html lang="en">
-            <body>
-                <MSWProvider>{children}</MSWProvider>
-            </body>
+            <body>{children}</body>
         </html>
     );
 }
