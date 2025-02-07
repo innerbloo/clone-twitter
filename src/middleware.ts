148c450c4a1715e8e6ccf2cdb82b46223cbff41d
Hosung <nsa19020@naver.com>
2025-02-07 16:50:08 +0900

    포트 변경

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,7 +5,7 @@ import { auth } from './auth';
 export async function middleware() {
     const session = await auth();
     if (!session) {
-        return NextResponse.redirect('http://localhost:3000/i/flow/login');
+        return NextResponse.redirect('http://localhost:80/i/flow/login');
     }
 }
 
